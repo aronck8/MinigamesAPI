894702e95c82d8a10774258f9a935b7c63fe000c
aronck <W_JR99]BzCF@K*Sx>
2024-03-10 23:16:15 +0100

    added possibility to set minigames configurations such as friendly fire etc.

---
+++ bukkit/src/main/java/com/aronck/minigamesapi/minigame/MinigameConfiguration.java
@@ -0,0 +1,12 @@
+package com.aronck.minigamesapi.minigame;
+
+public class MinigameConfiguration {
+
+    private boolean allowFriendlyFire = false;
+    private boolean allowNaturalMobSpawning = false;
+    private boolean allowNaturalFriendlyMobSpawning = false;
+    private boolean allowGameLightCycle = false;
+    private boolean allowMapDestroyByTNT = false;
+
+
+}
